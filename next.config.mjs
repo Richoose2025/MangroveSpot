8ba6f0746c43cfe3111914ce3e2e5aca74360f87
Adarsh-oo7 <adarshsarachandran@gmail.com>
2026-02-14 01:35:52 +0530

    adf

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // required for GitHub Pages
-
-  basePath: '/MangroveSpot',      // your repo name
-  assetPrefix: '/MangroveSpot/',  // VERY IMPORTANT
-
-  images: {
-    unoptimized: true, // required for static export
-  },
-
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 }
 
